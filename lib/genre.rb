df1ae92ebfb5923235ac63058c46516c784ca7bc
syd333 <sydmiranda@icloud.com>
2021-01-20 00:25:18 -0500

    done

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,24 @@
 
+class Genre
+
+    @@all =[]
+
+    attr_reader :name
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        songs.collect {|song| song.artist}
+    end
+end
